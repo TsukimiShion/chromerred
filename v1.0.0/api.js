392a31f5484fa1abbd59790451b029b20775692f
Tsukimi Shion <tsukimishion@gmail.com>
2014-10-19 16:45:24 +0900

    Release the version 1.0.0.

---
+++ v1.0.0/api.js
@@ -0,0 +1,12 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "chromerred.runtime",
+        "chromerred.storage",
+        "chromerred.storage.StorageArea",
+        "chromerred.tabs"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
